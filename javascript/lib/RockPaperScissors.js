a288ccf3e674b8cee3aba08c962ef8a8e1944668
babelberry <tatienantes@yahoo.fr>
2014-09-15 16:25:45 +0100

    added lizard test and funstionality

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,16 +1,9 @@
-var PAIRS = {
-	rock: 		{beats: "scissors" },
-	paper: 		{beats: "rock"},
-	scissors: 	{beats: "paper"}
-};
-
-
 function Player(name) {
   this.name = name;
 }
 
 Player.prototype.picks = function(pick) {
-	this.pick = pick
+	this.pick = pick;
 };
 
 function Game(player1, player2) {
@@ -24,19 +17,25 @@ function Game(player1, player2) {
 
 }
 
-Game.prototype.samePick = function() {
-	    if(this.player1.pick === this.player2.pick)
-	   		return true
+Game.prototype.PAIRS = {
+rock: 		{beats: ["lizard", "scissors"]},
+paper: 		{beats: ["rock"]},
+scissors: 	{beats: ["paper"]}
+};
+
+Game.prototype._isSamePick = function() {
+	    return this.player1.pick === this.player2.pick;
 };
 
 
 Game.prototype.winner = function() {
-	if(this.player1.pick === this.player2.pick)
-	   	return null
-    else if(PAIRS[this.player1.pick]["beats"] === this.player2.pick)
-    	return this.player1
-    else 
-    	return this.player2
+	if(this._isSamePick()) return null;
+    if(this.PAIRS[this.player1.pick].beats.indexOf(this.player2.pick) > -1){
+    	return this.player1;
+    }
+    else {
+    	return this.player2;
+    }
 };
 
 
--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -1,3 +1,10 @@
+// http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-Spock
+
+// 2nd extension task - Write a new method (with tests), that returns a string with the winning player's name, the winning player's pick, the losing player's name and pick, joined with an appropriate verb. E.g.
+
+
+
+
 describe("Rock-Paper-Scissors", function() {
   var player1, player2, game;
 
@@ -26,6 +33,22 @@ describe("Rock-Paper-Scissors", function() {
         player1.picks('rock');
         player2.picks('paper');
         expect(game.winner()).toBe(player2);
+
+      });
+
+        it('should lose to Spock', function() {
+
+        player1.picks('rock');
+        player2.picks('Spock');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+        it('should beat lizard', function() {
+
+        player1.picks('rock');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1);
 
       });
 
@@ -73,19 +96,21 @@ describe("Rock-Paper-Scissors", function() {
 
   });
 
+
+
   describe('draws', function() {
 
     describe('any identical picks', function() {
 
       it('should result in no winner', function() {
 
-        var drawGameResults = ['rock', 'paper', 'scissors'].map(function(x) {
+        var drawGameResults = ['rock', 'paper', 'scissors', 'lizard', 'Spock'].map(function(x) {
           player1.picks(x);
           player2.picks(x);
           return game.winner();
         });
 
-        expect(drawGameResults).toEqual([null, null, null]);
+        expect(drawGameResults).toEqual([null, null, null, null, null]);
 
       });
 
