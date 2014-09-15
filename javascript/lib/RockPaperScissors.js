81f5a71cce1be3d718fa0b6a50fbd4563430e69a
Jerome Pratt <jerome.pratt@outlook.com>
2014-09-15 13:00:58 +0100

    fixing tests in javascript

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,17 +1,48 @@
+var PAIRS = {
+	rock: 		{beats: "scissors" },
+	paper: 		{beats: "rock"},
+	scissors: 	{beats: "paper"}
+};
+
+
 function Player(name) {
   this.name = name;
 }
 
+Player.prototype.picks = function(pick) {
+	this.pick = pick
+};
+
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
+
+ //  function samePick() {
+	//   if(this.player1.pick === this.player2.pick)
+	//   	return true
+	// }
+
 }
 
-
-Player.prototype.picks = function(pick) {
-  // body...
+Game.prototype.samePick = function() {
+	    if(this.player1.pick === this.player2.pick)
+	   		return true
 };
+
 
 Game.prototype.winner = function() {
-    return this.player1;
+	if(this.player1.pick === this.player2.pick)
+	   	return null
+    else if(PAIRS[this.player1.pick]["beats"] === this.player2.pick)
+    	return this.player1
+    else 
+    	return this.player2
 };
+
+
+
+
+
+
+
+
